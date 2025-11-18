7185842479887ed740239a86d4953368885bbeb3
tonybevilacqua <tonybevilacqua23@gmail.com>
2025-11-17 17:28:56 -0800

    Remove hardcoded MySQL password - use environment variable

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
+import os
 from urllib.parse import quote_plus
 
 class Config:
-    password = quote_plus('sMtQDMpmhBYoiFf7EvB') 
+    password = quote_plus(os.environ.get('MYSQL_PASSWORD', 'default'))
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://root:{password}@localhost/mechanic_shop'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'  # Using SQLite instead of MySQL
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    import os
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
