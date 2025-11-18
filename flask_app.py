1f00cd93bab7ba82fbb08297af2abd640296482c
tonybevilacqua <tonybevilacqua23@gmail.com>
2025-11-17 17:53:39 -0800

    Remove dotenv import for production

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,5 @@
 from application import create_app
 from application.extensions import db
 import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 app = create_app('ProductionConfig')
-
